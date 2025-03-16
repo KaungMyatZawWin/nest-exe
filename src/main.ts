3af1fd323b7660721f028cf4e170ec466e0cc6dc
kaungmyatzawwin <>
2025-03-16 12:04:12 +0630

    feat: adding global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './response/response.interceptor';
+import { HttpExceptionFilter } from './exception/exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api/v1")
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true
   }))
